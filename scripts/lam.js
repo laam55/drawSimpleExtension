c8454f5c5ce8df04ffa9ce7c49791a5c419d5cbe
hoho91111 <hoho91111@gmail.com>
2019-11-21 21:38:25 +0700

    modify somthing

--- scripts/lam.js
+++ scripts/lam.js
@@ -1,10 +1,19 @@
-function $(el) {
-    return document.getElementById(el.replace(/#/, ''));
-};
-var canvas = $('#show');
+setTimeout(() => {
+    document.getElementById('alert').style.display = 'none'
+}, 3000)
+
+var canvas = document.getElementById('show');//)$('#show');
 canvas.width = 342;
 canvas.height = 342;
+
+// canvas
 var context = canvas.getContext('2d');
+
+context.beginPath();
+context.rect(0, 0, 342, 342);
+context.fillStyle = "#eeeeee";
+context.fill();
+
 var start = function(coors) {
     context.beginPath();
     context.moveTo(coors.x, coors.y);
@@ -47,3 +56,22 @@ canvas.addEventListener('mouseup', draw, false);
 canvas.addEventListener('touchstart', draw, false);
 canvas.addEventListener('touchmove', draw, false);
 canvas.addEventListener('touchend', draw, false);
+
+// download image from canvas
+function downloadImageFromCanvas(e) {
+    if ((event.ctrlKey || event.metaKey) && String.fromCharCode(event.which).toLowerCase() == 's') {
+        event.preventDefault();
+
+        // var d = canvas.toDataURL("image/png");
+        // var w = window.open('about:blank','image from canvas');
+        // w.document.write("<img src='" + d + "' alt='from canvas'/>");
+
+        let downloadLink = document.createElement('a');
+        downloadLink.setAttribute('download', 'image.png');
+        let dataURL = canvas.toDataURL('image/png');
+        let url = dataURL.replace(/^data:image\/png/,'data:application/octet-stream');
+        downloadLink.setAttribute('href', url);
+        downloadLink.click();
+    }
+}
+document.addEventListener('keydown', downloadImageFromCanvas)
